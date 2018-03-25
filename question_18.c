fca2528b3b9b24a6bb633f49b557736d70ca4134
Aditya Kumar <kumar.sava.aditya@gmail.com>
2018-03-26 00:19:51 +0530

    Ques. 18: Add student structure and input

--- question_18.c
+++ question_18.c
@@ -8,7 +8,22 @@
  */
 
 #include <stdio.h>
+#define N 10
+
+struct student {
+	char name;
+	int arrival_time;
+	int gifts_bought;
+};
 
 int main() {
-	
+	struct student students[N];
+	for(int i = 0; i<N; i++) {
+		students[i].name = 'A'+i;
+		printf("Enter gifts bought by %c: ", students[i].name);
+		scanf("%d", &(students[i].gifts_bought));
+		printf("Enter arrival time (in minutes) of %c: ", students[i].name);
+		scanf("%d", &(students[i].arrival_time));
+		students[i]
+	}
 }
