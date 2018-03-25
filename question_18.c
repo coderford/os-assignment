5575b6cd1349527b5b90892e971f28930d5e4969
Aditya Kumar <kumar.sava.aditya@gmail.com>
2018-03-26 01:06:41 +0530

    Solve question 18

--- question_18.c
+++ question_18.c
@@ -8,22 +8,57 @@
  */
 
 #include <stdio.h>
+#include <limits.h>
 #define N 10
 
 struct student {
 	char name;
 	int arrival_time;
 	int gifts_bought;
+	int billed;
 };
 
 int main() {
 	struct student students[N];
-	for(int i = 0; i<N; i++) {
+	int billed_count = 0;
+	int i;
+		
+	// get input:
+	for(i = 0; i<N; i++) {
 		students[i].name = 'A'+i;
 		printf("Enter gifts bought by %c: ", students[i].name);
 		scanf("%d", &(students[i].gifts_bought));
 		printf("Enter arrival time (in minutes) of %c: ", students[i].name);
 		scanf("%d", &(students[i].arrival_time));
-		students[i]
+		students[i].billed = 0;
 	}
+
+	printf("\nThe billing order will be as follows:\n");
+
+	// now calculate billing order and print:
+	while(billed_count<N) {
+		int to_bill = 0;
+		int min_arrival = INT_MAX;
+		int max_gifts = 0;
+		for(int i = 0; i<N; i++) {
+			if(!students[i].billed) {
+				if(students[i].arrival_time < min_arrival) {
+					to_bill = i;
+					min_arrival = students[i].arrival_time;
+					max_gifts = students[i].gifts_bought;
+				}
+				if(students[i].arrival_time == min_arrival) {
+					if(students[i].gifts_bought > max_gifts) {
+						to_bill = i;
+						max_gifts = students[i].gifts_bought;
+					}
+				}
+			}
+		}
+		students[to_bill].billed = 1;
+		billed_count++;
+		printf("%c ", students[to_bill].name);
+	}
+	printf("\n");
+	return 0;
 }
